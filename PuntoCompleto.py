d5bda420393d7c97329ccf11e679d37b85278265
Juan Felipe Rave C <jfrcorrea10@hotmail.com>
2017-05-04 18:07:46 -0500

    cambio a variable datos

--- PuntoCompleto.py
+++ PuntoCompleto.py
@@ -19,6 +19,7 @@ t1 = 0
 t2 = 0
 lcd = CharLCD(cols=16, rows=2, pin_rs=37, pin_e=35, pins_data=[33, 31, 29, 23])
 mensaje = 'Pulsa para empezar'
+datos = ''
 
 #Evento de interrupcion
 def interrupcion(pulsador):
